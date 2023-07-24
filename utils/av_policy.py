8cc8dea591bcc078ed17e9747f9ea015f5de6576
YizhouXu-THU <1609740947@qq.com>
2023-07-24 16:35:09 +0800

    modified:   utils/av_policy.py

--- utils/av_policy.py
+++ utils/av_policy.py
@@ -238,7 +238,7 @@ class RL_brain:
         action = self.policy_net.choose_action(state, deterministic=deterministic)
         return action.cpu().numpy()
 
-    def train(self) -> dict[str, float]:
+    def train(self, auto_alpha=True) -> dict[str, float]:
         state, action, reward, next_state, not_done = self.replay_buffer.sample(batch_size=self.batch_size)
         new_action, log_prob = self.policy_net.evaluate(state)
 
@@ -267,22 +267,25 @@ class RL_brain:
 
         # update parameters
         self.value_optimizer.zero_grad()
-        self.q1_optimizer.zero_grad()
-        self.q2_optimizer.zero_grad()
-        self.policy_optimizer.zero_grad()
-        self.alpha_optimizer.zero_grad()
-
         value_loss.backward()
-        q1_value_loss.backward()
-        q2_value_loss.backward()
-        policy_loss.backward()
-        alpha_loss.backward()
-
         self.value_optimizer.step()
+        
+        self.q1_optimizer.zero_grad()
+        q1_value_loss.backward()
         self.q1_optimizer.step()
+        
+        self.q2_optimizer.zero_grad()
+        q2_value_loss.backward()
         self.q2_optimizer.step()
+        
+        self.policy_optimizer.zero_grad()
+        policy_loss.backward()
         self.policy_optimizer.step()
-        self.alpha_optimizer.step()
+        
+        if auto_alpha:
+            self.alpha_optimizer.zero_grad()
+            alpha_loss.backward()
+            self.alpha_optimizer.step()
         
         # soft update of target network
         for target_param, param in zip(self.target_value_net.parameters(), self.value_net.parameters()):
