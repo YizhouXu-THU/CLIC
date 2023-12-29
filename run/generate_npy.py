24d10f9f16aa1c4ec1b13c374ff9a548fe2899ef
YizhouXu-THU <1609740947@qq.com>
2023-12-29 14:17:49 +0800

    modified:   README.md 	modified:   run/generate_npy.py

--- run/generate_npy.py
+++ run/generate_npy.py
@@ -17,7 +17,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 random_seed = 42
 set_random_seed(random_seed)
 
-lib = scenario_lib(path='./data/example/')
+lib = scenario_lib(path='./data/all/')
 env = Env(max_bv_num=lib.max_bv_num, cfg_sumo='./config/lane.sumocfg', gui=sumo_gui)
 av_model = RL_brain(env, capacity=0, device=device)
 
@@ -26,7 +26,7 @@ success_rate = 1 - np.sum(gt_label) / gt_label.size
 print('Success rate: %.4f' % success_rate)
 
 scenario_data = np.append(lib.data, gt_label.reshape(-1,1), axis=1)
-# np.save('./data/example.npy', scenario_data)
-np.savez('./data/example.npz', data=scenario_data, type_count=lib.type_count, max_bv_num=lib.max_bv_num)
+# np.save('./data/all.npy', scenario_data)
+np.savez('./data/all.npz', data=scenario_data, type_count=lib.type_count, max_bv_num=lib.max_bv_num)
 
 env.close()
