21f7263d5b9c425cc123a162a68075372b56d42e
YizhouXu-THU <1609740947@qq.com>
2024-06-05 21:01:42 +0800

    modified:   run/result/scenario_analysis.py

--- run/result/scenario_analysis.py
+++ run/result/scenario_analysis.py
@@ -4,7 +4,7 @@ root_path = os.getcwd()
 sys.path.append(root_path)
 
 from tqdm import trange
-from math import atan2, sqrt, pi, atan2
+from math import sqrt, pi, atan2
 import random
 import numpy as np
 import matplotlib.pyplot as plt
@@ -43,41 +43,41 @@ for i in trange(lib.scenario_num):
                                               (scenario[1 + t * bv_num + j, 3] - scenario[1 + t * bv_num + k, 3]) ** 2))
 
 # highD dataset
-bv_bv_dis_highd, bv_yaw_highd, bv_vel_highd, bv_acc_highd = [], [], [], []
-for file in os.listdir(highd_path):
-    if file.endswith('tracks.csv'):
-        print(file)
-        data = np.loadtxt(highd_path+file, delimiter=',', skiprows=1)
-        max_timestep = int(np.max(data[:, 0]))
-        veh_num = int(np.max(data[:, 1]))
-        for i in range(data.shape[0]):
-            if data[i, 6] > 0:
-                yaw = atan2(data[i, 7], data[i, 6]) * 180 / pi
-            elif data[i, 6] < 0:
-                yaw = -atan2(data[i, 7], -data[i, 6]) * 180 / pi
-            bv_yaw_highd.append(yaw)
-            bv_vel_highd.append(sqrt(data[i, 6] ** 2 + data[i, 7] ** 2))
-            bv_acc_highd.append(sqrt(data[i, 8] ** 2 + data[i, 9] ** 2))
-        for t in trange(max_timestep):
-            scene = data[data[:, 0] == t+1]
-            for i in range(scene.shape[0]):
-                for j in range(i+1, scene.shape[0]):
-                    bv_bv_dis_highd.append(sqrt((scene[i, 2] - scene[j, 2]) ** 2 + (scene[i, 3] - scene[j, 3]) ** 2))
+# bv_bv_dis_highd, bv_yaw_highd, bv_vel_highd, bv_acc_highd = [], [], [], []
+# for file in os.listdir(highd_path):
+#     if file.endswith('tracks.csv'):
+#         print(file)
+#         data = np.loadtxt(highd_path+file, delimiter=',', skiprows=1)
+#         max_timestep = int(np.max(data[:, 0]))
+#         veh_num = int(np.max(data[:, 1]))
+#         for i in range(data.shape[0]):
+#             if data[i, 6] > 0:
+#                 yaw = atan2(data[i, 7], data[i, 6]) * 180 / pi
+#             elif data[i, 6] < 0:
+#                 yaw = -atan2(data[i, 7], -data[i, 6]) * 180 / pi
+#             bv_yaw_highd.append(yaw)
+#             bv_vel_highd.append(sqrt(data[i, 6] ** 2 + data[i, 7] ** 2))
+#             bv_acc_highd.append(sqrt(data[i, 8] ** 2 + data[i, 9] ** 2))
+#         # for t in trange(max_timestep):
+#         #     scene = data[data[:, 0] == t+1]
+#         #     for i in range(scene.shape[0]):
+#         #         for j in range(i+1, scene.shape[0]):
+#         #             bv_bv_dis_highd.append(sqrt((scene[i, 2] - scene[j, 2]) ** 2 + (scene[i, 3] - scene[j, 3]) ** 2))
 
 
 plt.figure(figsize=(4, 3))
-plt.hist(np.array(bv_bv_dis_lib, dtype=np.float16), bins=50, alpha=0.8, density=True, label='scenario library')
-plt.hist(np.array(bv_bv_dis_highd, dtype=np.float16), bins=50, alpha=0.8, density=True, label='highD dataset')
-plt.legend()
+plt.hist(np.array(bv_bv_dis_lib), bins=50, alpha=0.6, density=True, label='scenario library')
+# plt.hist(np.array(bv_bv_dis_highd), bins=50, alpha=0.6, density=True, label='highD dataset')
+# plt.legend()
 plt.title('BV-BV distance distribution')
 plt.xlabel('distance(m)')
 plt.ylabel('frequency')
 plt.savefig('./figure/bv_bv_dis.pdf', bbox_inches='tight')
 
 plt.figure(figsize=(4, 3))
-plt.hist(np.array(bv_av_dis_lib, dtype=np.float16), bins=25, alpha=0.8, density=True, label='scenario library')
-plt.hist(np.array(bv_bv_dis_highd, dtype=np.float16), bins=25, alpha=0.8, density=True, label='highD dataset')
-plt.legend()
+plt.hist(np.array(bv_av_dis_lib), bins=25, alpha=0.6, density=True, label='scenario library')
+# plt.hist(np.array(bv_bv_dis_highd), bins=25, alpha=0.6, density=True, label='highD dataset')
+# plt.legend()
 plt.title('BV-AV distance distribution')
 plt.xlabel('distance(m)')
 plt.ylabel('frequency')
@@ -88,8 +88,7 @@ index = random.sample(range(len(bv_av_pos_x_lib)), 2000)
 bv_av_pos_x_lib = [bv_av_pos_x_lib[i] for i in index]
 bv_av_pos_y_lib = [bv_av_pos_y_lib[i] for i in index]
 plt.figure(figsize=(9, 1.4))
-plt.scatter(np.array(bv_av_pos_x_lib, dtype=np.float16), np.array(bv_av_pos_y_lib, dtype=np.float16), 
-            s=2, alpha=0.5, label='BV')
+plt.scatter(np.array(bv_av_pos_x_lib), np.array(bv_av_pos_y_lib), s=2, alpha=0.5, label='BV')
 plt.scatter(0, 0, s=20, c='r', label='AV')
 plt.grid()
 plt.legend()
@@ -100,30 +99,31 @@ plt.ylabel('y(m)')
 plt.savefig('./figure/bv_av_pos.pdf', bbox_inches='tight')
 
 plt.figure(figsize=(4, 3))
-plt.hist(np.array(bv_yaw_lib, dtype=np.float16), bins=50, alpha=0.8, density=True, label='scenario library')
-plt.hist(np.array(bv_yaw_highd, dtype=np.float16), bins=50, alpha=0.8, density=True, label='highD dataset')
-plt.legend()
+plt.hist(np.array(bv_yaw_lib), bins=50, alpha=0.6, density=True, label='scenario library')
+# plt.hist(np.array(bv_yaw_highd), bins=50, alpha=0.6, density=True, label='highD dataset')
+# plt.legend()
+plt.xlim(-30, 30)
 plt.title('BV yaw distribution')
 plt.xlabel(r'yaw($^\circ$)')
 plt.ylabel('frequency')
 plt.savefig('./figure/bv_yaw.pdf', bbox_inches='tight')
 
 plt.figure(figsize=(4, 3))
-plt.hist(np.array(bv_vel_lib, dtype=np.float16), bins=50, alpha=0.8, density=True, label='scenario library')
-plt.hist(np.array(bv_vel_highd, dtype=np.float16), bins=50, alpha=0.8, density=True, label='highD dataset')
-plt.legend()
+plt.hist(np.array(bv_vel_lib), bins=50, alpha=0.6, density=True, label='scenario library')
+# plt.hist(np.array(bv_vel_highd), bins=50, alpha=0.6, density=True, label='highD dataset')
+# plt.legend()
 plt.title('BV velocity distribution')
 plt.xlabel('velocity(m/s)')
 plt.ylabel('frequency')
 plt.savefig('./figure/bv_vel.pdf', bbox_inches='tight')
 
 plt.figure(figsize=(4, 3))
-plt.hist(np.array(bv_acc_lib, dtype=np.float16), bins=50, alpha=0.8, density=True, label='scenario library')
-plt.hist(np.array(bv_acc_highd, dtype=np.float16), bins=50, alpha=0.8, density=True, label='highD dataset')
-plt.legend()
+plt.hist(np.array(bv_acc_lib), bins=50, alpha=0.6, density=True, label='scenario library')
+# plt.hist(np.array(bv_acc_highd), bins=50, alpha=0.6, density=True, label='highD dataset')
+# plt.legend()
+plt.xlim(-9, 9)
+plt.xticks(np.arange(-8, 10, 4))
 plt.title('BV acceration distribution')
 plt.xlabel(r'acceleration(m/s$^2$)')
 plt.ylabel('frequency')
-plt.xlim(-9, 9)
-plt.xticks(np.arange(-8, 10, 4))
 plt.savefig('./figure/bv_acc.pdf', bbox_inches='tight')
